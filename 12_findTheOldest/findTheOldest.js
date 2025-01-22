c51d56660bdc873219da2e752a5a08eecfccfd1f
andersonflexa <andersonflexamedeiros@gmail.com>
2025-01-22 18:04:46 -0300

    All excercises done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        return personAge > oldestAge ? person : oldest;
+    });
 };
+
+const people = [
+    {
+        name: 'Carly',
+        yearOfBirth: 1942,
+        yearOfDeath: 1970
+    },
+    {
+        name: 'Ray',
+        yearOfBirth: 1962,
+        yearOfDeath: 2011
+    },
+    {
+        name: 'Jane',
+        yearOfBirth: 1912,
+        yearOfDeath: 1941
+    },
+    {
+        name: 'John',
+        yearOfBirth: 1990
+    }
+]
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
